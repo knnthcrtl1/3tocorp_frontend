1216fd8722facfd4d1b9f72b9d4a3da00601e6f9
unknown <kcartel@gofluent.com>
2019-10-03 18:07:12 +0800

    Updates

---
+++ precache-manifest.a9f655d0c607cb21183a966dcbf2ff87.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dab85348193dbcd7d18a5638f4bfd711",
+    "url": "/3tocorp_frontend/index.html"
+  },
+  {
+    "revision": "37db2f1b09503990f528",
+    "url": "/3tocorp_frontend/static/css/4.e13e0823.chunk.css"
+  },
+  {
+    "revision": "4e2fc71c36941351387f",
+    "url": "/3tocorp_frontend/static/css/5.fa01e1a4.chunk.css"
+  },
+  {
+    "revision": "12d3b24370564f3612dc",
+    "url": "/3tocorp_frontend/static/css/6.ce84c8f7.chunk.css"
+  },
+  {
+    "revision": "5306b857d9fef7cd51b1",
+    "url": "/3tocorp_frontend/static/css/main.6e988398.chunk.css"
+  },
+  {
+    "revision": "ab745a49eb8fd89756bf",
+    "url": "/3tocorp_frontend/static/js/0.4ede7ac8.chunk.js"
+  },
+  {
+    "revision": "471073a67c981d1e7ca1",
+    "url": "/3tocorp_frontend/static/js/3.ea1c031e.chunk.js"
+  },
+  {
+    "revision": "37db2f1b09503990f528",
+    "url": "/3tocorp_frontend/static/js/4.5ed6a1e0.chunk.js"
+  },
+  {
+    "revision": "4e2fc71c36941351387f",
+    "url": "/3tocorp_frontend/static/js/5.1af16a2b.chunk.js"
+  },
+  {
+    "revision": "12d3b24370564f3612dc",
+    "url": "/3tocorp_frontend/static/js/6.dc469902.chunk.js"
+  },
+  {
+    "revision": "5306b857d9fef7cd51b1",
+    "url": "/3tocorp_frontend/static/js/main.9af443d3.chunk.js"
+  },
+  {
+    "revision": "b8f5a8f18a80035a87ea",
+    "url": "/3tocorp_frontend/static/js/runtime~main.0d6f9361.js"
+  },
+  {
+    "revision": "731a28a413d642522667a2de8681ff35",
+    "url": "/3tocorp_frontend/static/media/Poppins-Regular.731a28a4.ttf"
+  },
+  {
+    "revision": "cfce86c1c32ffd349e9cf9cd37a83bc7",
+    "url": "/3tocorp_frontend/static/media/loginImg.cfce86c1.png"
+  },
+  {
+    "revision": "736d57447b9c16ba4eedcd7a78042c1a",
+    "url": "/3tocorp_frontend/static/media/strategy.736d5744.png"
+  },
+  {
+    "revision": "68481a9aa1b18a0dabbfae6acae328ff",
+    "url": "/3tocorp_frontend/static/media/study.68481a9a.png"
+  }
+]);
